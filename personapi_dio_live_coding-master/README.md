9beda5b95c45ed0f401dfbf925650c5dab08d7d5
Sávio André <savioa834@gmail.com>
2021-08-30 17:26:38 -0300

    Add files via upload

---
+++ personapi_dio_live_coding-master/README.md
@@ -0,0 +1,3 @@
+# Sistema de gerenciamento de pessoas em API REST com Spring Boot
+
+> by Sávio André
---
+++ personapi_dio_live_coding-master/src/test/java/one/digitalinnovation/personapi/utils/PersonUtils.java
@@ -0,0 +1,37 @@
+package one.digitalinnovation.personapi.utils;
+
+import one.digitalinnovation.personapi.dto.request.PersonDTO;
+import one.digitalinnovation.personapi.entity.Person;
+
+import java.time.LocalDate;
+import java.util.Collections;
+
+public class PersonUtils {
+
+    private static final String FIRST_NAME = "Savio";
+    private static final String LAST_NAME = "Andre";
+    private static final String CPF_NUMBER = "000.000.000-00";
+    private static final long PERSON_ID = 1L;
+    public static final LocalDate BIRTH_DATE = LocalDate.of(2003, 25, 03);
+
+    public static PersonDTO createFakeDTO() {
+        return PersonDTO.builder()
+                .firstName(FIRST_NAME)
+                .lastName(LAST_NAME)
+                .cpf(CPF_NUMBER)
+                .birthDate("05-06-2000")
+                .phones(Collections.singletonList(PhoneUtils.createFakeDTO()))
+                .build();
+    }
+
+    public static Person createFakeEntity() {
+        return Person.builder()
+                .id(PERSON_ID)
+                .firstName(FIRST_NAME)
+                .lastName(LAST_NAME)
+                .cpf(CPF_NUMBER)
+                .birthDate(BIRTH_DATE)
+                .phones(Collections.singletonList(PhoneUtils.createFakeEntity()))
+                .build();
+    }
+}
